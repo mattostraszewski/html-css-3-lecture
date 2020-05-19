a89b06a8b15c20f9c2ed17f5308d9b4745f6b91a
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-05-19 15:11:01 -0600

    lecture prep

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <div className="logo-hold">
+            <img
+              alt="Logo"
+              src="https://i.ya-webdesign.com/images/stack-of-boxes-png-8.png"
+              className="logo-img"
+            />
+            <p className="logo-text">BOXES R US</p>
+          </div>
+          <nav className="nav">
+            <ul className="nav-list">
+              <li className="nav-list-item">Home</li>
+              <li className="nav-list-item">About</li>
+              <li className="nav-list-item">Contact</li>
+            </ul>
+          </nav>
+        </header>
+        <div className="boxes-hold">
+          <div className="box box-1"></div>
+          <div className="box box-2"></div>
+          <div className="box box-3"></div>
+        </div>
+        <div className="boxes-hold">
+          <div className="box box-4"></div>
+          <div className="box box-5"></div>
+          <div className="box box-6"></div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
